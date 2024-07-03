a941dc15b5db6cc9a667f508a90b79c62c5e073b
owusuprince <94824951+owusuprince@users.noreply.github.com>
2024-07-03 06:01:51 -1200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ backgroundColor: '#00fff' }}>
+      <Text style={{ fontSize: 24 }}>
+        My name is <Text style={{ fontWeight: 'bold' }}>Muheeb Nuaku</Text>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
